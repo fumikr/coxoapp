b6331edea2dcc66e92de31b0d27f9f42f835e456
Glitch (ezsfbmaster) <none>
2018-08-07 15:38:35 +0000

    🍜👼 Updated with Glitch v2.1.0

--- public/history.js
+++ public/history.js
@@ -37,8 +37,7 @@ function getFetch(action, data) {
     success: function (response) {
       if(response.success) {
         $container.html('');
-        $container.append('<table id="HistoryTable" class="tablesorter"><col width="150"><col width="60"><thead><tr><th class="header">達人用戶</th><th class="header">發佈<br>帖數</th><th class="header">粉絲反應數目</th></tr></thead><tbody></tbody>');
-        console.log(response);
+        $container.append('<table id="HistoryTable" class="tablesorter"><col id="col1" width="150"><col id="col2" width="75"><col id="col3"><col id="col4" width="75"><col id="col5" width="75"><thead><tr><th class="header">達人用戶</th><th class="header">發佈<br>帖數</th><th class="header">粉絲反應數目</th><th class="header">返回<br>數目</th><th class="header">GTR</th></tr></thead><tbody></tbody>');
         for (var i= 0;  i < response.users.length; i++) {
           var user = response.users[i];
           var adjacency = Object.values(user.adjacency);
@@ -48,11 +47,14 @@ function getFetch(action, data) {
              fanList += '<div class="fans-tag"><img src="' + adjacency[j].avatar + '" title="' 
                + adjacency[j].name +'">' + adjacency[j].reaction_count + '</div>'; 
           };
+          var gtr = (user.num_of_reacts)/ (user.num_of_posts + 1);
           $('#HistoryTable > tbody').append('<tr><td><img src="' 
                                             + user.avatar + '"><br>' 
                                             + user.name + '</a></td><td>' 
                                             + user.num_of_posts + '</td><td><div class="flex-wrap">'
-                                            + fanList + '</div></td></tr>');
+                                            + fanList + '</div></td><td>'
+                                            + user.num_of_reacts + '</td><td>'
+                                            + gtr.toFixed(2) + '</td></tr>');
         };
         $("#HistoryTable").tablesorter({sortList: [[0,0]]}); 
       } else {
